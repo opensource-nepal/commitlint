d092c97235bcbfc8d1117ae7f96799fd6ef684a9
Ajesh Sen Thapa <aj3sshh@gmail.com>
2024-01-14 22:59:06 +0545

    feat: added strict regex for validation and ignored auto commits

---
+++ src/commitlint/messages.py
@@ -0,0 +1,21 @@
+"""
+This module provides constant messages used in the application for various scenarios.
+"""
+
+from .constants import COMMIT_MAX_LENGTH
+
+COMMIT_SUCCESSFUL = "Commit validation: successful!"
+
+CORRECT_OUTPUT_FORMAT = (
+    "Correct commit format:\n"
+    "---------------------------------------\n"
+    "<type>(<optional scope>): <description>\n"
+    "---------------------------------------\n"
+    "For more details visit "
+    "https://www.conventionalcommits.org/en/v1.0.0/"
+)
+INCORRECT_FORMAT_ERROR = (
+    "Commit message does not follow conventional commits format."
+    f"\n{CORRECT_OUTPUT_FORMAT}"
+)
+HEADER_LENGTH_ERROR = f"Header must not be longer than {COMMIT_MAX_LENGTH} characters."
